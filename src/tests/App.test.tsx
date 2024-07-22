280304278b606ff6736daa228660683f9d966d50
azmusgb <36898133+azmusgb@users.noreply.github.com>
2024-07-22 19:51:37 -0400

    Update App.test.tsx

--- src/tests/App.test.tsx
+++ src/tests/App.test.tsx
@@ -2,10 +2,8 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from '../App';
 
-test('renders FormWorks Agent Service Script text', () => {
+test('renders FormWorks Utility Suite text', () => {
   render(<App />);
-  const linkElement = screen.getByText((content, element) => {
-    return /FormWorks Agent Service Script/i.test(content);
-  });
+  const linkElement = screen.getByText(/FormWorks Utility Suite/i);
   expect(linkElement).toBeInTheDocument();
 });
