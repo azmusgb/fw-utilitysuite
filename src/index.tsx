476984631b561b67d6a4f82ef343bf18c4c67459
azmusgb <36898133+azmusgb@users.noreply.github.com>
2024-07-21 20:35:11 -0400

    Create index.tsx

---
+++ src/index.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+import ErrorBoundary from './components/ErrorBoundary';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <ErrorBoundary>
+      <App />
+    </ErrorBoundary>
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
+reportWebVitals(console.log);
