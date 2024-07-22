c679f735a0fc9dd0e77dcf61c10a431346800eb6
azmusgb <36898133+azmusgb@users.noreply.github.com>
2024-07-22 00:24:39 -0400

    Update index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import ErrorBoundary from './components/common/ErrorBoundary';
 
 ReactDOM.render(
   <React.StrictMode>
-    <ErrorBoundary>
-      <App />
-    </ErrorBoundary>
+    <App />
   </React.StrictMode>,
   document.getElementById('root')
 );
 
-reportWebVitals(console.log);
+reportWebVitals();
