05687c923e21f50ce30ef7cd6a3023109998d591
azmusgb <36898133+azmusgb@users.noreply.github.com>
2024-07-21 20:31:46 -0400

    Create Tools.tsx

---
+++ src/components/common/Tools.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+/**
+ * Tools component displaying various tools for the application.
+ * 
+ * @returns {JSX.Element} The rendered Tools component.
+ */
+const Tools: React.FC = () => {
+  return (
+    <div>
+      <h1>Tools</h1>
+      <p>This page will contain various tools for managing configurations and DCNs.</p>
+    </div>
+  );
+};
+
+export default Tools;
