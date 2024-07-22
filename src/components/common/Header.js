0861987f590817a3e3c77bbb56fd142741415dc7
azmusgb <36898133+azmusgb@users.noreply.github.com>
2024-07-21 20:57:35 -0400

    Delete src directory

--- src/components/common/Header.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const Header = () => (
-  <header>
-    <h1>FormWorks Agent Service Script</h1>
-  </header>
-);
-
-export default Header;
--- src/tests/Header.test.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import Header from '../components/common/Header';
-
-test('renders Header component', () => {
-  render(<Header />);
-  expect(screen.getByText(/FW Utility Suite/i)).toBeInTheDocument();
-});
