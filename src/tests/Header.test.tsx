9ccf203e00e013ab30e699d5acbd4ec6d9271d9e
azmusgb <36898133+azmusgb@users.noreply.github.com>
2024-07-21 21:57:38 -0400

    Update Header.test.tsx

--- src/tests/Header.test.tsx
+++ src/tests/Header.test.tsx
@@ -4,5 +4,6 @@ import Header from '../components/common/Header';
 
 test('renders Header component', () => {
   render(<Header />);
-  expect(screen.getByText(/FW Utility Suite/i)).toBeInTheDocument();
+  const headerElement = screen.getByText(/FormWorks Agent Service Script/i);
+  expect(headerElement).toBeInTheDocument();
 });
