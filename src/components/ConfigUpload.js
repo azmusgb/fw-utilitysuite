069f6ba04b862427bbe022c414eb0744bf48d1bf
azmusgb <36898133+azmusgb@users.noreply.github.com>
2024-07-22 19:58:17 -0400

    Update ConfigUpload.js

--- src/components/ConfigUpload.js
+++ src/components/ConfigUpload.js
@@ -2,24 +2,11 @@ import React from 'react';
 
 function ConfigUpload() {
   return (
-    <div className="config-upload">
-      <form>
-        <label htmlFor="configFile">Upload Configuration File:</label>
-        <input
-          accept=".cfg"
-          id="configFile"
-          name="configFile"
-          type="file"
-        />
-        <button className="form-button" type="submit">Upload</button>
-      </form>
-      <div id="configUploadResults" />
-      <div className="loading" id="configUploadLoading" style={{ display: 'none' }}>
-        <div className="loading-spinner" />
-        <p>Loading...</p>
-      </div>
-      <div className="feedback" id="configUploadFeedback" role="alert" />
-    </div>
+    <form>
+      <label htmlFor="configFile">Upload Configuration File:</label>
+      <input id="configFile" type="file" />
+      {/* Other form elements */}
+    </form>
   );
 }
 
