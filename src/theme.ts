06ff09d2296b0027860aacda6ef96966766234fd
azmusgb <36898133+azmusgb@users.noreply.github.com>
2024-07-22 00:25:59 -0400

    Update theme.ts

---
+++ src/theme.ts
@@ -0,0 +1,16 @@
+import { DefaultTheme } from 'styled-components';
+
+const theme: DefaultTheme = {
+  colors: {
+    primary: '#4CAF50', // Adjust based on your design
+    secondary: '#f9f9f9',
+    text: '#333',
+    background: '#f9f9f9',
+    link: '#4CAF50',
+  },
+  fonts: {
+    main: 'Segoe UI, Tahoma, Geneva, Verdana, sans-serif',
+  },
+};
+
+export default theme;
