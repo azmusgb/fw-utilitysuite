06d7643242db51c1993c4c60a1261fa885e4542b
azmusgb <36898133+azmusgb@users.noreply.github.com>
2024-07-22 16:10:49 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,60 +1,29 @@
-
-import React, { useState } from 'react';
+import React from 'react';
 import './App.css';
-import ServiceStatus from './components/ServiceStatus';
 import ConfigUpload from './components/ConfigUpload';
-import SATCommand from './components/SATCommand';
-import WorkRecord from './components/WorkRecord';
 import DCNCreation from './components/DCNCreation';
-import SavedWRs from './components/SavedWRs';
-import Tools from './components/Tools';
 import FAQ from './components/FAQ';
+import SATCommand from './components/SATCommand';
+import SavedWRs from './components/SavedWRs';
+import ServiceStatus from './components/ServiceStatus';
 import Settings from './components/Settings';
-import Chart from './components/Chart';
-import Header from './components/common/Header';
+import Tools from './components/Tools';
+import WorkRecord from './components/WorkRecord';
 import Navigation from './components/common/Navigation';
 
 function App() {
-  const [activeTab, setActiveTab] = useState('serviceStatus');
-
-  const renderTabContent = () => {
-    switch (activeTab) {
-      case 'serviceStatus':
-        return <ServiceStatus />;
-      case 'configUpload':
-        return <ConfigUpload />;
-      case 'satCommand':
-        return <SATCommand />;
-      case 'workRecord':
-        return <WorkRecord />;
-      case 'dcnCreation':
-        return <DCNCreation />;
-      case 'savedWRs':
-        return <SavedWRs />;
-      case 'tools':
-        return <Tools />;
-      case 'faq':
-        return <FAQ />;
-      case 'settings':
-        return <Settings />;
-      case 'chart':
-        return <Chart />;
-      default:
-        return <ServiceStatus />;
-    }
-  };
-
   return (
     <div className="App">
-      <Header />
-      <Navigation activeTab={activeTab} setActiveTab={setActiveTab} />
-      <div className="parallax"></div>
-      <div className="content">
-        {renderTabContent()}
-      </div>
-      <footer>
-        <p>&copy; 2024 FormWorks Utility Suite - All rights reserved</p>
-      </footer>
+      <Navigation />
+      <ConfigUpload />
+      <DCNCreation />
+      <FAQ />
+      <SATCommand />
+      <SavedWRs />
+      <ServiceStatus />
+      <Settings />
+      <Tools />
+      <WorkRecord />
     </div>
   );
 }
