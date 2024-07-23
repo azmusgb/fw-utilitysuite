a79b808d568b010db0680f49a4ae26adb077baa9
azmusgb <36898133+azmusgb@users.noreply.github.com>
2024-07-22 20:01:47 -0400

    Update Header.tsx

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,11 +1,9 @@
 import React from 'react';
 
-const Header: React.FC = () => {
-  return (
-    <header>
-      <h1>FormWorks Utility Suite</h1>
-    </header>
-  );
-};
+const Header: React.FC = () => (
+  <header>
+    <h1>FormWorks Utility Suite</h1>
+  </header>
+);
 
 export default Header;
