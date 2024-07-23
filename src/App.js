9610f34f03bfcac67b2c10e84586e447426f30af
azmusgb <36898133+azmusgb@users.noreply.github.com>
2024-07-22 20:07:16 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import Header from './components/Header.tsx';  // Ensure correct extension
+import Header from './components/Header';
 
 function App() {
   return (
@@ -8,8 +8,10 @@ function App() {
       <form>
         <label htmlFor="input1">Input 1:</label>
         <input id="input1" type="text" />
+        
         <label htmlFor="input2">Input 2:</label>
         <input id="input2" type="text" />
+        
         <label htmlFor="input3">Input 3:</label>
         <input id="input3" type="text" />
       </form>
