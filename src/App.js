e0c094bbac78d3ab31d7d617f01cee793a5ae66d
azmusgb <36898133+azmusgb@users.noreply.github.com>
2024-07-23 06:51:43 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -5,16 +5,6 @@ function App() {
   return (
     <div className="App">
       <Header />
-      <form>
-        <label htmlFor="input1">Input 1:</label>
-        <input id="input1" type="text" />
-
-        <label htmlFor="input2">Input 2:</label>
-        <input id="input2" type="text" />
-
-        <label htmlFor="input3">Input 3:</label>
-        <input id="input3" type="text" />
-      </form>
     </div>
   );
 }
